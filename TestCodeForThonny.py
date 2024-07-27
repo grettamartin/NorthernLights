7cfbd917a63979b1736e3ec9d6bcf7a387d23a09
grettamartin <grettajmartin@gmail.com>
2024-07-27 15:44:59 -0500

    Changed code because I found that the actual kp index is logged in a JSON. Altered code to grab that content and store current kp index in variable current_kp

--- TestCodeForThonny.py
+++ TestCodeForThonny.py
@@ -1,15 +1,40 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 import requests
 
-pageToScrape = requests.get("https://www.swpc.noaa.gov/products/planetary-k-index#")
-soup = BeautifulSoup(pageToScrape.text, "html.parser")
-spanContent = soup.findAll("span")
-#print(spanContent)
-for span in spanContent:
-    if "Estimated Planetary K index" in span.text:
-        kpIndex = span.text
-        kpIndex = kpIndex.split(":")[-1].strip()
-        print(kpIndex)
-print(spanContent)
+json_url = "https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json"
+
+pageToScrape = requests.get(json_url)
+raw_data = pageToScrape.json()
+
+headers = raw_data[0]
+rows = raw_data[1:]
+
+print(headers)
+print(rows)
+
+kp_index_position = headers.index("Kp")
+
+size = 0
+kpList = []
+
+for row in rows:
+    time_tag = row[0]
+    kp_value = row[kp_index_position]
+    size += 1
+    kpList.append(kp_value)
+
+current_kp = kpList[size-1]
+print(current_kp)
+    
+    #print(time_tag, kp_value)
+
+# spanContent = soup.findAll("span")
+# #print(spanContent)
+# for span in spanContent:
+#     if "Estimated Planetary K index" in span.text:
+#         kpIndex = span.text
+#         kpIndex = kpIndex.split(":")[-1].strip()
+#         print(kpIndex)
+# print(spanContent)
 
