293c181df094918e946b05dfdc49335b51a8964d
grettamartin <grettajmartin@gmail.com>
2024-07-31 22:54:58 -0500

    Added code to handle coronal mass ejection data from Cactus .txt url

--- TestCodeForThonny.py
+++ TestCodeForThonny.py
@@ -26,15 +26,47 @@ for row in rows:
 
 current_kp = kpList[size-1]
 print(current_kp)
-    
-    #print(time_tag, kp_value)
 
-# spanContent = soup.findAll("span")
-# #print(spanContent)
-# for span in spanContent:
-#     if "Estimated Planetary K index" in span.text:
-#         kpIndex = span.text
-#         kpIndex = kpIndex.split(":")[-1].strip()
-#         print(kpIndex)
-# print(spanContent)
+#checks if cme has been detected within past 48 hours
+cme_II_presence = "false"
+cme_III_presence = "false"
+cme_IV_presence = "false"
+
+text_url = "https://www.sidc.be/cactus/out/cmecat.txt"
+response = requests.get(text_url)
+
+content = response.text
+lines = content.splitlines()
+
+date_list = [] #index 0
+da_list = [] #index
+
+line_count = 0
+for line in lines:
+    line_count += 1
+    if (line_count >= 28) and (line_count <= 50) :
+        #print(line[2])
+        split_line = line.split('|')
+        date = split_line[1].strip()
+        date_list.append(date)
+        da = split_line[4].strip()
+        da_list.append(da)
+ 
+da_index = 0
+for da in da_list:
+    int_da = int(da)
+    da_index +=0
+    if int_da > 270:
+        cme_IV_presence = "true"
+    elif int_da > 180:
+        cme_III_presence = "true"
+    elif int_da > 90:
+        cme_II_presence = "true"
+
+if cme_IV_presence == "true":
+    print("There has recently been a coronal mass ejection with a type IV halo.")
+if cme_III_presence == "true":
+    print("There has recently been a coronal mass ejection with a type III halo.")
+if cme_II_presence == "true":
+    print("There has recently been a coronal mass ejection with a type II halo.")
 
